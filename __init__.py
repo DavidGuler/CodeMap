499f91eb64daea6d72026c11b310fde3017b2e36
DrunkFox <davidguler.1x@gmail.com>
2018-09-23 14:17:24 +0300

    Removed the viewer and builder folders.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 __all__ = []
 
-from CodeViewer.core.builder.builder import Builder
+from CodeViewer.core.builder import Builder
 from CodeViewer.elements.code_connections import *
 from CodeViewer.elements.code_elements import *
